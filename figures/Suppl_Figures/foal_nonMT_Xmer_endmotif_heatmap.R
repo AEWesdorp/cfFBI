8f68b39e7fae9f22d240baeecf6821a5d650090d
AEWesdorp <a.e.wesdorp@umcutrecht.nl>
2024-06-28 16:12:55 +0200

    update size figure

--- figures/Suppl_Figures/foal_nonMT_Xmer_endmotif_heatmap.R
+++ figures/Suppl_Figures/foal_nonMT_Xmer_endmotif_heatmap.R
@@ -30,7 +30,7 @@ foal_nonMT_1mer_endmotif_heatmap <- foal_nonMT_IS_meta %>%
         theme_bw() + 
         scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0) + 
         ylab("1-mer end motif") + xlab("foals") + labs(fill = "log10,\no/e-ratio") + 
-        theme(axis.text.x = element_text(angle = 90, hjust = 1), strip.background = element_blank()) 
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5), strip.background = element_blank()) 
 ggsave("../../output_figures/foal_nonMT_1mer_endmotif_heatmap.png", plot = foal_nonMT_1mer_endmotif_heatmap, 
        width = 7, height = 3, units = "in")
 ggsave("../../output_figures/foal_nonMT_1mer_endmotif_heatmap.pdf", plot = foal_nonMT_1mer_endmotif_heatmap, 
@@ -55,9 +55,9 @@ foal_nonMT_2mer_endmotif_heatmap <- foal_nonMT_IS_meta %>%
         theme_bw() + 
         scale_fill_gradient2(low = "red", mid = "white", high = "blue", midpoint = 0)+ 
         ylab("2-mer end motif") + xlab("foals") + labs(fill = "log10,\no/e-ratio") + 
-        theme(axis.text.x = element_text(angle = 90, hjust = 1), strip.background = element_blank()) 
+        theme(axis.text.x = element_text(angle = 90, hjust = 0.5), strip.background = element_blank()) 
 ggsave("../../output_figures/foal_nonMT_2mer_endmotif_heatmap.png", plot = foal_nonMT_2mer_endmotif_heatmap, 
-       width = 7, height = 3, units = "in")
+       width = 7, height = 7, units = "in")
 ggsave("../../output_figures/foal_nonMT_2mer_endmotif_heatmap.pdf", plot = foal_nonMT_2mer_endmotif_heatmap, 
-       width = 7, height = 3, units = "in")
+       width = 7, height = 7, units = "in")
 print("Done, part 2 of 2! Figures can be found here: ../../output_figures/foal_nonMT_2mer_endmotif_heatmap.*")
